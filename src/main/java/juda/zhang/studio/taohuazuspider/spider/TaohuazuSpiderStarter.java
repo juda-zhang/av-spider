c28dafb34335421b281ddec2f8eb6f9e16091170
zhangchenhui160 <zhangchenhui160@jk.cn>
2017-06-07 15:24:07 +0800

    添加spring boot

--- src/main/java/juda/zhang/studio/taohuazuspider/spider/TaohuazuSpiderStarter.java
+++ src/main/java/juda/zhang/studio/taohuazuspider/spider/TaohuazuSpiderStarter.java
@@ -2,11 +2,15 @@ package juda.zhang.studio.taohuazuspider.spider;
 
 import juda.zhang.studio.taohuazuspider.spider.pipline.TaohuazuPipline;
 import juda.zhang.studio.taohuazuspider.spider.processor.TaohuazuPageProcessor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import us.codecraft.webmagic.Spider;
 import us.codecraft.webmagic.monitor.SpiderMonitor;
 import us.codecraft.webmagic.pipeline.ConsolePipeline;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import javax.annotation.Resource;
 import javax.management.JMException;
 
@@ -15,21 +19,19 @@ import javax.management.JMException;
  */
 @Service
 public class TaohuazuSpiderStarter {
-
     public static final int THREAD_NUM = 1;
-
     public static final String START_URL = "http://taohuabbs.cc/forum-220-1.html";
+    private final static Logger LOGGER = LoggerFactory.getLogger(TaohuazuSpiderStarter.class);
     @Resource
     private TaohuazuPageProcessor taohuazuPageProcessor;
     @Resource
     private TaohuazuPipline taohuazuPipline;
 
-    public TaohuazuSpiderStarter() {
-        //run();
-    }
+    private Spider taohuazuSpider;
 
+    @PostConstruct
     private void run() {
-        Spider taohuazuSpider = Spider.create(taohuazuPageProcessor)
+        taohuazuSpider = Spider.create(taohuazuPageProcessor)
                 .thread(THREAD_NUM)
                 .addUrl(START_URL)
                 .addPipeline(new ConsolePipeline())
@@ -37,8 +39,16 @@ public class TaohuazuSpiderStarter {
         try {
             SpiderMonitor.instance().register(taohuazuSpider);
         } catch (JMException e) {
-            e.printStackTrace();
+            LOGGER.error("taohuazu-spider监控注册失败!", e);
         }
         taohuazuSpider.start();
+        taohuazuSpider.stop();
+        LOGGER.info("taohuazu-spider启动完毕!");
+    }
+
+    @PreDestroy
+    public void destroy() {
+        taohuazuSpider.stop();
+        LOGGER.info("taohuazu-spider关闭完毕！");
     }
 }
---
+++ src/test/java/juda/zhang/studio/taohuazuspider/MainTest.java
@@ -0,0 +1,17 @@
+package juda.zhang.studio.taohuazuspider;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Created by zhangchenhui160 on 2017/6/7.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Main.class)
+public class MainTest {
+    @Test
+    public void testMain() {
+    }
+}
