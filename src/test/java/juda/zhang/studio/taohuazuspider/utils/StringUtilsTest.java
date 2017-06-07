eadaf194a5b7c73855a5cf48ba8172530baff6e5
zhangchenhui160 <zhangchenhui160@jk.cn>
2017-06-07 15:30:17 +0800

    添加spring boot

---
+++ src/test/java/juda/zhang/studio/taohuazuspider/utils/StringUtilsTest.java
@@ -0,0 +1,15 @@
+package juda.zhang.studio.taohuazuspider.utils;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by zhangchenhui160 on 2017/6/7.
+ */
+public class StringUtilsTest {
+    @Test
+    public void testTrimAndUppder() {
+        String s = StringUtils.trimAndUpper(" aAA ");
+        Assert.assertEquals(s, "AAA");
+    }
+}
