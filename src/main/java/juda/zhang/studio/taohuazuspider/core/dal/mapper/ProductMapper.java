447ff127917575c3c68919626798dd3a7b65d515
zhangchenhui160 <zhangchenhui160@jk.cn>
2017-06-09 18:01:57 +0800

    sqlmapping

--- src/main/java/juda/zhang/studio/taohuazuspider/core/dal/mapper/ProductMapper.java
+++ src/main/java/juda/zhang/studio/taohuazuspider/core/dal/mapper/ProductMapper.java
@@ -1,7 +1,15 @@
 package juda.zhang.studio.taohuazuspider.core.dal.mapper;
 
+import juda.zhang.studio.taohuazuspider.core.model.ProductDO;
+
 /**
  * Created by zhangchenhui160 on 2017/6/9.
  */
 public interface ProductMapper {
+
+    void insert(ProductDO productDO);
+
+    int update(ProductDO productDO);
+
+    void deleteById(long id);
 }
