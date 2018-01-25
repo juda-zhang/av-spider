fad5154bf356a7ff6613ee6e5d9374bd7e14054d
Juda <juda.zhang@aliyun.com>
2018-01-26 00:07:09 +0800

    update user-agent in order to cheat the website

--- src/main/java/juda/zhang/studio/avspider/spider/taohuazu/processor/ThzAisaCensoredDetailPageProcessor.java
+++ src/main/java/juda/zhang/studio/avspider/spider/taohuazu/processor/ThzAisaCensoredDetailPageProcessor.java
@@ -27,7 +27,7 @@ import java.util.stream.Collectors;
  */
 @Service("thzAisaCensoredDetailPageProcessor")
 public class ThzAisaCensoredDetailPageProcessor implements PageProcessor {
-    private static final String USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_2) AppleWebKit/537.31 (KHTML, like Gecko) Chrome/26.0.1410.65 Safari/537.31";
+    private static final String USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36";
     private final static Logger LOGGER = LoggerFactory.getLogger(ThzAisaCensoredDetailPageProcessor.class);
     @Value("${site.domain}")
     private String DOMAIN;
